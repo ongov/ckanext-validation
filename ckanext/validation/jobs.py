57d20fad35b7f931176cd64d462ec95accecb2b8
katiRG <cnangini@gmail.com>
2023-12-01 09:34:27 -0500

    WIP refine first char check

--- ckanext/validation/jobs.py
+++ ckanext/validation/jobs.py
@@ -174,7 +174,7 @@ def _validate_table(source, _format='csv', schema=None, **options):
                           schema=resource_schema,
                           checks=[header_rule_2_2_header_length(),
                                   header_rule_2_3_snake_case(),
-                                  header_rule_2_4_underscore(),
+                                  header_rule_2_4_first_char(),
                                   data_entry_rule_2_6_bullet_lists()])
         log.debug('Validating source: %s', source)
 
@@ -244,7 +244,7 @@ class header_rule_2_3_snake_case(Check):
                                                 field_number=field_number+1,
                                                 field_name=header)
 
-class header_rule_2_4_underscore(Check):
+class header_rule_2_4_first_char(Check):
     ''' 
     Column headers must begin with an alphabetic letter. 
     '''
@@ -263,16 +263,18 @@ class header_rule_2_4_underscore(Check):
 # Data entry rules
 class data_entry_rule_2_6_bullet_lists(Check):
     ''' 
-    Cell value cannot contain a bullet list. 
+    Cell value cannot contain a bullet list.
+    Skip check for non-string cells.
     '''
     Errors = [errors.ForbiddenValueError]
     def validate_row(self, row):
         for header in list(row):
             this_cell = row[header]
-            list_chars = [u'•', u'*', u'- ', u'â€”', u'\n',  u'\t'] # u'â€”' is em-dash
-            bullet_check = [ele for ele in list_chars if(ele in this_cell)]
-            if len(bullet_check) > 0:
-                note = 'Cell value cannot contain bullets, dashes, new line or tab characters. Please make separate rows.'
-                yield errors.ForbiddenValueError.from_row(row, 
-                                                        note=note,
-                                                        field_name=header)
+            if isinstance(this_cell, str):
+                list_chars = [u'•', u'* ', u'- ', u'â€”', u'\n',  u'\t'] # u'â€”' is em-dash
+                bullet_check = [ele for ele in list_chars if(ele in this_cell.rstrip())] # strip any trailing spaces
+                if len(bullet_check) > 0:
+                    note = 'Cell value cannot contain bullets, dashes, new line or tab characters. Please make separate rows.'
+                    yield errors.ForbiddenValueError.from_row(row, 
+                                                            note=note,
+                                                            field_name=header)
